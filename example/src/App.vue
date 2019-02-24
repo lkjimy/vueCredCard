dd0f0ddc1e6553f9190ce8064ecd0322db95ad64
thiagoN13 <thiago.nogueira187@gmail.com>
2019-02-23 23:08:37 -0300

    [UPD] Added dynamic update

--- example/src/App.vue
+++ example/src/App.vue
@@ -2,36 +2,37 @@
   <div id="app" class="container-fluid">
     <div class="row">
       <div class="col-12 d-flex justify-content-center">
-        <vue-cred-card :card-number="cardNumber" ref="credCard"></vue-cred-card>
+        <vue-cred-card
+          :card-number="cardNumber"
+          :cvv="cvv"
+          :expiration-year="expirationDate"
+          :expiration-month="expirationDate"
+          :credit-card-holder-name="creditCardHolderName"
+          ref="credCard">
+        </vue-cred-card>
       </div>
     </div>
   
     <div class="row justify-content-center text-center mt-5">
       <div class="col-12 col-sm-10 col-md-9 col-lg-7 col-xl-6 px-sm-0">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Holder name" @focus="setTurnAroundInverted">
+          <input type="text" class="form-control" v-model="creditCardHolderName" placeholder="Holder name" @focus="setTurnAroundInverted">
         </div>
   
         <div class="form-group">
-          <input type="text" class="form-control" v-model="cardNumber" placeholder="Credit card number" @focus="setTurnAroundInverted">
+          <input type="text" class="form-control" maxlength="16" v-model="cardNumber" placeholder="Credit card number" @focus="setTurnAroundInverted">
         </div>
   
         <div class="form-row">
-          <div class="col-3">
+          <div class="col-5">
             <div class="form-group">
-              <input type="date" class="form-control" @focus="setTurnAroundInverted">
+              <input type="month" v-model="expirationDate" class="form-control" @focus="setTurnAroundInverted">
             </div>
           </div>
   
-          <div class="col-3">
+          <div class="col-3 offset-4">
             <div class="form-group">
-              <input type="date" class="form-control" @focus="setTurnAroundInverted">
-            </div>
-          </div>
-  
-          <div class="col-3 offset-3">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="CVV" @focus="setTurnAround">
+              <input type="number" class="form-control" v-model="cvv" placeholder="CVV" @focus="setTurnAround">
             </div>
           </div>
   
@@ -51,7 +52,10 @@
     },
     data () {
       return {
-        cardNumber: ''
+        expirationDate: null,
+        creditCardHolderName: null,
+        cardNumber: null,
+        cvv: null
       }
     },
     methods: {
