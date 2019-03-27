5a4366721356d04e725eb58350d87919f383ecc5
thiagoN13 <thiago.nogueira187@gmail.com>
2019-03-27 00:11:15 -0300

    [FIX] Remove limit url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,6 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         query: {
-          limit: 10000,
           name: 'images/[name].[hash:7].[ext]'
         }
       },
